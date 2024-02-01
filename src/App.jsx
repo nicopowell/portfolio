06ff448f6ee5c7586a7ff3fa8c523b4ce492751e
Nicolas Powell <nicopowell30@gmail.com>
2024-02-01 18:35:50 -0300

    Import bootstrap and include the base navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,6 @@
-import './App.css'
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "react-bootstrap";
 import Header from './components/common/Header'
 import Main from './components/Main'
 import Footer from './components/common/Footer'
