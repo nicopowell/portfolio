3201546342d11b52d53dc7558279b5f2742e17e1
Nicolas Powell <nicopowell30@gmail.com>
2024-02-01 19:24:03 -0300

    i added the brand logo to the basic footer

--- src/components/common/Footer.jsx
+++ src/components/common/Footer.jsx
@@ -1,7 +1,32 @@
+import "./Footer.css";
+import logoFooter from "../../assets/Logo Light.png";
+
 const Footer = () => {
     return (
-        <footer>
-            Footer
+        <footer className="container-fluid pt-3">
+            <section className="row">
+                <aside className="col-md-6 ps-lg-5 col-xs-12 fw-bold fs-4 d-flex align-items-center">
+                    <a href={"/"}>
+                        <img src={logoFooter} id="logoFooter" alt="logoFooter"></img>
+                        <span className="ms-2">Nico Powell</span>
+                    </a>
+                </aside>
+                <aside className="col-md-6 text-md-end col-xs-12">
+                    <a href={"/"}>
+                        <img src={logoFooter} id="logoFooter" alt="logoFooter"></img>
+                    </a>
+                    <a href={"/"}>
+                        <img src={logoFooter} id="logoFooter" alt="logoFooter"></img>
+                    </a>
+                    <a href={"/"}>
+                        <img src={logoFooter} id="logoFooter" alt="logoFooter"></img>
+                    </a>
+                </aside>
+            </section>
+            <hr className="text-white" />
+            <section className="text-light text-center">
+                <p>&copy; Todos los derechos reservados</p>
+            </section>
         </footer>
     );
 };
