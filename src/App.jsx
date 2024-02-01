cea61f975ed23c521195318249121aa20d038e6c
Nicolas Powell <nicopowell30@gmail.com>
2024-02-01 16:49:51 -0300

    Send the footer to the bottom of the page

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,18 @@
 import './App.css'
+import Header from './components/common/Header'
+import Main from './components/Main'
+import Footer from './components/common/Footer'
 
 function App() {
 
 
   return (
-    <>
-    </>
+  <>
+    <Header></Header>
+    <Main></Main>
+    <Footer></Footer>
+  </>
+    
   )
 }
 
---
+++ src/components/Main.jsx
@@ -0,0 +1,11 @@
+import { Container } from "react-bootstrap";
+
+const Main = () => {
+    return (
+        <Container  className="mainSection">
+            Main
+        </Container>
+    );
+};
+
+export default Main;
