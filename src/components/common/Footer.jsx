56b570aa48aa400b7b2353eec500d1ef5cb71025
Nicolas Powell <nicopowell30@gmail.com>
2024-02-01 21:32:26 -0300

    fix footer icons

--- src/components/common/Footer.jsx
+++ src/components/common/Footer.jsx
@@ -1,5 +1,8 @@
 import "./Footer.css";
 import logoFooter from "../../assets/Logo Light.png";
+import Github from "../../assets/Github-Icon.avif";
+import Linkedin from "../../assets/Linkedin-Icon.avif";
+import Instagram from "../../assets/Instagram-Icon.avif";
 
 const Footer = () => {
     return (
@@ -11,10 +14,15 @@ const Footer = () => {
                         <span className="fw-semibold fs-4 ms-2">Nico Powell</span>
                     </a>
                 </aside>
-                <aside className="pe-sm-5">
-                    <a href={"/"} className="d-flex align-items-center">
-                        <img src={logoFooter} id="logoFooter" alt="logoFooter"></img>
-                        <span className="fw-semibold fs-4 ms-2">Nico Powell</span>
+                <aside className="pe-sm-5 d-flex gap-3">
+                    <a href={"https://github.com/nicopowell"} target="blank" className="d-flex align-items-center">
+                        <img src={Github} className="mediaLogoFooter" alt="logoFooter"></img>
+                    </a>
+                    <a href={"https://www.linkedin.com/in/nicolas-powell-938a47249/"} target="blank" className="d-flex align-items-center">
+                        <img src={Linkedin} className="mediaLogoFooter" alt="logoFooter"></img>
+                    </a>
+                    <a href={"https://www.instagram.com/nicopowell30/"} target="blank" className="d-flex align-items-center">
+                        <img src={Instagram} className="mediaLogoFooter" alt="logoFooter"></img>
                     </a>
                 </aside>
             </section>
