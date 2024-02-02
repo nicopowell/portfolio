22ac65532969d684fbd2fed6c1c44877da4d5bd4
Nicolas Powell <nicopowell30@gmail.com>
2024-02-02 18:15:48 -0300

    added hr tags to split sections

---
+++ src/components/Projects.jsx
@@ -0,0 +1,13 @@
+const Projects = () => {
+  return (
+    <section
+      id="projects"
+      className="d-flex flex-column text-center mt-5 py-5 Container"
+    >
+      <h2 className="title fs-1 ">Projects</h2>
+      <p className="lead mt-4 mb-5">Here you will find some of my projects</p>
+    </section>
+  );
+};
+
+export default Projects;
