5cd51c387e98b9de39ac257622c45abf8c32f589
Nicolas Powell <nicopowell30@gmail.com>
2024-02-02 00:13:11 -0300

    added the tech icons

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,3 +1,13 @@
+import "./About.css";
+import htmlIcon from "../assets/HTML.avif";
+import cssIcon from "../assets/CSS.avif";
+import jsIcon from "../assets/JS.avif";
+import reactIcon from "../assets/React.avif";
+import mongoIcon from "../assets/MongoDB.avif";
+import expressIcon from "../assets/ExpressJS.avif";
+import nodeIcon from "../assets/NodeJS.avif";
+import gitIcon from "../assets/Git.avif";
+
 const About = () => {
     return (
         <section id="about" className=" d-flex flex-column text-center mt-5 py-5">
@@ -6,10 +16,10 @@ const About = () => {
                 Here you will find more information about me, what I do, and my current skills
                 mostly in terms of programming and technology
             </p>
-            <div className="row mt-4">
-                <aside className="col-12 col-lg-6 text-start">
-                    <h3 className="title">A little bit about me</h3>
-                    <p className="lead mt-3">
+            <div className="row mt-4 text-start">
+                <aside className="col-12 col-lg-6 pe-lg-5">
+                    <h3 className="title mb-3">A little bit about me</h3>
+                    <p className="lead">
                         Passionate full-stack developer specialized in crafting efficient and
                         visually appealing web solutions.
                     </p>
@@ -22,7 +32,23 @@ const About = () => {
                         Ready to collaborate on innovative projects! Contact me.
                     </p>
                 </aside>
-                <aside className="col-12 col-lg-6">awd</aside>
+                <aside className="col-12 col-lg-6 ps-lg-5">
+                    <h3 className="title mb-3">My Skills</h3>
+                    <div className="d-flex gap-4 pb-3 flex-wrap justify-content-center justify-content-lg-start">
+                        <div className="d-flex gap-4">
+                            <img src={htmlIcon} alt="" className="aboutIcons" />
+                            <img src={cssIcon} alt="" className="aboutIcons" />
+                            <img src={jsIcon} alt="" className="aboutIcons" />
+                            <img src={reactIcon} alt="" className="aboutIcons" />
+                        </div>
+                        <div  className="d-flex gap-4">
+                            <img src={mongoIcon} alt="" className="aboutIcons" />
+                            <img src={expressIcon} alt="" className="aboutIcons" />
+                            <img src={nodeIcon} alt="" className="aboutIcons" />
+                            <img src={gitIcon} alt="" className="aboutIcons" />
+                        </div>
+                    </div>
+                </aside>
             </div>
         </section>
     );
